2adfea5524999b87dced5c238b1b50a2c05b457f
Rupam Mondal <mondalrupam756@gmil.com>
2026-07-07 21:38:31 +0530

    commitrd

---
+++ EnglishWord.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string words[] = {"zero", "one", "two", "three", "four",
+                      "five", "six", "seven", "eight", "nine"};
+
+    if (n >= 1 && n <= 9)
+        cout << words[n];
+    else
+        cout << "Greater than 9";
+
+    return 0;
+}
